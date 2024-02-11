69f61e4439c1ce0c0c2854a1c8138f73e920770b
Heyson Betancourt <146369401+heysonbr@users.noreply.github.com>
2024-02-11 17:34:52 +0000

    acabado

--- src/js/layout.js
+++ src/js/layout.js
@@ -29,7 +29,7 @@ const Layout = () => {
           <Routes>
             <Route path="/form" element={<ContactForm />} />
             <Route path="/" element={<ContactList />} />
-            <Route path="/edit/:contactId" element={<ContactForm />} />
+
             {/* <Route path="/" element={<Home />} /> */}
             {/* <Route path="/demo" element={<Demo />} />
             <Route path="/single/:theid" element={<Single />} /> */}
